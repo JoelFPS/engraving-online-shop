e962f99c58f7e076c2fc3a2aab3962d67ff40e46
JoelFPS <j.achremczyk@gmail.com>
2023-10-02 12:18:42 +0200

    nav norbert

---
+++ src/Components/Navbar.js
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../Styles/main.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>Grawerowy.pl</h3>
+			<nav ref={navRef}>
+				<a href="/#">Prezenty</a>
+				<a href="/#">Długopisy</a>
+				<a href="/#">Noże</a>
+				<a href="/#">O nas</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
