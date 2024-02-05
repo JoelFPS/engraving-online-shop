79200892451ea60f5cfef10d022075bea8784ed0
JoelFPS <j.achremczyk@gmail.com>
2024-02-05 14:28:44 +0100

    complete the orderSchema

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -3,16 +3,41 @@ const mongoose = require("mongoose");
 const orderSchema = mongoose.Schema({
         username: {
             type: String,
-            required: [true, "Please add the product name"]
+            required: [true, "Please add the username"]
         },
         totalPrice: {
             type: Number,
-            required: [true, "Please add the product price"]
+            required: [true, "Please add the total price"]
+        },
+        city: {
+            type: String,
+            required: [true, "Please add the city name"]
+        },
+        street: {
+            type: String,
+            required: [true, "Please add the street name"]
+        },
+        number: {
+            type: Number,
+            required: [true, "Please add the house's number"]
+        },
+        apartmentNumber: {
+            type: Number,
+            required: [false, "Please add the apartment number"]
+        },
+        zipCode: {
+            type: String,
+            required: [true, "Please add the zip code"]
+        },
+        products: {
+            type: Object,
+            required: [true, "Please add the products"]
         }
+        
     },
     {
         timestamps: true,
     }
     )
 
-    module.exports = mongoose.model("Product", orderSchema);
+    module.exports = mongoose.model("Order", orderSchema);
