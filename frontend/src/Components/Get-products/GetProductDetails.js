5c8c59a7c266698c1f677fce832a0cef33f5dd65
Norbert <norbertbindas@gmail.com>
2024-03-18 13:06:15 +0100

    getProduct to function

--- frontend/src/Components/Get-products/GetProductDetails.js
+++ frontend/src/Components/Get-products/GetProductDetails.js
@@ -1,71 +1,60 @@
 import React, { Component } from 'react';
 import CartContext from '../../Context/Cart/CartContext';
 import { useContext } from 'react';
-import "../../Styles/product.scss"
+import "../../Styles/product.scss";
+import { useState, useEffect } from 'react';
 
-export class GetProduct extends Component {
-    constructor() {
-        super();
-        this.state = {
-            productId: "",
-            productData: null
-        };
-        this.getProduct = this.getProduct.bind(this);
-    }
+function GetProduct(props) {
+    const [productId, setProductId] = useState("");
+    const [productData, setProductData] = useState(null);
     
-    componentDidMount() {
-        this.getProduct();
-    }
+    const { productId: productIdProp } = props;
+    const { addToCart, increase, cartItems, sumItems, itemCount } = useContext(CartContext);
 
-    getProduct() {
-        const { productId } = this.props;
-        const { addToCart, increase, cartItems, sumItems, itemCount } =
-        useContext(CartContext);
+    useEffect(() => {
+        getProduct();
+    }, []);
+
+    const getProduct = () => {
         const requestOptions = {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
         };
-        console.log("cokolwiek")
 
-        fetch(`http://localhost:5001/api/products/${productId}`, requestOptions)
+        fetch(`http://localhost:5001/api/products/${productIdProp}`, requestOptions)
             .then(response => response.json())
             .then(data => {
-                this.setState({
-                    productData: data
-                });
+                setProductData(data);
             })
             .catch(error => {
                 console.error('Error fetching product:', error);
             });
-            console.log(this.state.productData);
-    }
+    };
 
-    render() {
-        return (
-            <div className='product-get'>
-                {this.state.productData && (
-                        <div className='product-page'>
-                            <div className='pg-left'>
-                                <img className="productPgImg" src={this.state.productData.src+".png"}></img>
-                            </div>
-                            <div className='pg-middle'>
-                                <h1>{this.state.productData.name}</h1>
-                                <div className='category'>Kategoria: {this.state.productData.category}</div>
-                                <div className='rating'>Opinie {this.state.productData.rating}</div>
-                                <div className='description'>{this.state.productData.description}</div>
-                            </div>
-                            <div className='pg-right'>
-                                    <h1 className='price'>{this.state.productData.price+" zł"}</h1>
-                                    <input className='quantity' type='number' id='quantity' defaultValue='1'/>
-                                    <input className='addToCartBtn' type='button' value='Dodaj do koszyka' />
-                                    {/*<input type='button' onClick={} value='Przejdź do koszyka' />*/}
-                            </div>
-                            <div className='clear-both'></div>
-                        </div>
-                )}
-            </div>
-        );
-    }
+    return (
+        <div className='product-get'>
+            {productData && (
+                <div className='product-page'>
+                    <div className='pg-left'>
+                        <img className="productPgImg" src={productData.src + ".png"} alt={productData.name} />
+                    </div>
+                    <div className='pg-middle'>
+                        <h1>{productData.name}</h1>
+                        <div className='category'>Kategoria: {productData.category}</div>
+                        <div className='rating'>Opinie {productData.rating}</div>
+                        <div className='description'>{productData.description}</div>
+                    </div>
+                    <div className='pg-right'>
+                        <h1 className='price'>{productData.price + " zł"}</h1>
+                        <input className='quantity' type='number' id='quantity' defaultValue='1' />
+                        <input className='addToCartBtn' type='button' value='Dodaj do koszyka' />
+                        {/*<input type='button' onClick={} value='Przejdź do koszyka' />*/}
+                    </div>
+                    <div className='clear-both'></div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default GetProduct;
